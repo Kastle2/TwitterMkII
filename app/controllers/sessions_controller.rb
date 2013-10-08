d54ad8ab08679914039d50a0e615a1ea43fead4a
Alan Donohoe <alandonohoe123@gmail.com>
2013-10-08 15:57:37 +0100

    altered session code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,19 +3,22 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	 user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    #  this line works with "form_for" in new.html.erb
+  	 # user = User.find_by(email: params[:session][:email].downcase)
+    
+    user = User.find_by_email(params[:email].downcase)
+    if user && user.authenticate(params[:password])
       # Sign the user in and redirect to the user's show page.
       sign_in user
       redirect_to user
     else
-		flash.now[:error] = 'Invalid email/password combination'
-      	render 'new'
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
     end
 
   end
 
-def destroy
+  def destroy
     sign_out
     redirect_to root_url
   end
